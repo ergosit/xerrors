c4b7dd5c1ba251d360cd020f8c5ba951a4e0c38f
Pavel Tsayukov <p.tsayukov@gmail.com>
2026-02-02 14:23:29 +0300

    📝 Add doc.go with package documentation

---
+++ doc.go
@@ -0,0 +1,5 @@
+// This file is licensed under the terms of the MIT License (see LICENSE file)
+// Copyright (c) 2026 Pavel Tsayukov p.tsayukov@gmail.com
+
+// Package xerrors extends the errors package from the standard library.
+package xerrors
